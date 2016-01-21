3ff6ec67a7f76da42660c9f74c2c1500003724f7
ss013 <ss013@PC013>
2016-01-21 12:06:22 +0900

    20160121_1206

--- src/algorithm/tsp/TwoOpt.java
+++ src/algorithm/tsp/TwoOpt.java
@@ -11,44 +11,54 @@ public class TwoOpt implements TSPAlgorithm {
 
 	@Override
 	public void sort(ArrayList<Location> list) {
-		
+
 		// 1. sort with NearestNeighbor
 		NearestNeighbor nn = new NearestNeighbor();
 		nn.sort(list);
-		
+
 		// 2. 2-opt
-		  // A. search crossed path
-		for(int basePointIndex=0; basePointIndex<list.size()-1; basePointIndex++){
-			Point basePathP1 = list.get(basePointIndex).getPoint();
-			Point basePathP2 = list.get(basePointIndex+1).getPoint();
-			for(int comparedPointIndex=0; comparedPointIndex<list.size()-1; comparedPointIndex++){
-				Point comparedPathP1 = list.get(comparedPointIndex).getPoint();
-				Point comparedPathP2 = list.get(comparedPointIndex+1).getPoint();
+		  // A. search crossing path
+		for(int i=0; i<list.size(); i++){
+			int basePathP1Index = i;
+			int basePathP2Index = ((i+1)>=list.size()) ? 0 : i+1;
+			Point basePathP1 = list.get(basePathP1Index).getPoint();
+			Point basePathP2 = list.get(basePathP2Index).getPoint();
+			for(int j=0; j<list.size(); j++){
+				int comparedPathP1Index = j;
+				int comparedPathP2Index =((j+1)>=list.size()) ? 0 : j+1;
+				Point comparedPathP1 = list.get(comparedPathP1Index).getPoint();
+				Point comparedPathP2 = list.get(comparedPathP2Index).getPoint();
 				if(basePathP1.equals(comparedPathP1)
 						|| basePathP1.equals(comparedPathP2)
 						|| basePathP2.equals(comparedPathP1)
 						|| basePathP2.equals(comparedPathP2)){
 					continue;
 				}
-				
-				// B. if crossed, reverse between basePointP2 to comparedPointP1
+
+				// B. if crossing, reverse between basePointP2 to comparedPointP1
 				if(AlgorithmUtilities.isIntersect(basePathP1, basePathP2, comparedPathP1, comparedPathP2)){
-					System.out.println(basePointIndex + " to " + (basePointIndex+1) + " x " + comparedPointIndex + " to " + (comparedPointIndex+1));
-					ArrayList<Location> tmpList = new ArrayList<Location>();
-					for(int index=basePointIndex+1; index<=comparedPointIndex; index++){
-						tmpList.add(list.get(index));
+					System.out.println("cross Path : " + basePathP1Index + "---" + basePathP2Index + " x " + comparedPathP1Index + "---" + comparedPathP2Index);
+					ArrayList<Location> toReverseList = new ArrayList<Location>();
+					System.out.println("basePathP1Index : " + basePathP1Index);
+					System.out.println("basePathP2Index : " + basePathP2Index);
+					System.out.println("comparedPathP1Index : " + comparedPathP1Index);
+					System.out.println("comparedPathP2Index : " + comparedPathP2Index);
+					for(int k=basePathP2Index; k!=comparedPathP2Index; ++k, k=(k>=list.size() ? 0 : k)){
+						toReverseList.add(list.get(k));
 					}
-					Collections.reverse(tmpList);
+					System.out.println("reverse, size = " + toReverseList.size());
+					Collections.reverse(toReverseList);
 					// C. set reverseList
-					for(int i=basePointIndex+1, j=0; i<=comparedPointIndex; i++, j++){
-						list.set(i, tmpList.get(j));
+					for(int l=basePathP2Index, m=0; l!=comparedPathP2Index; ++l, l=(l>=list.size() ? 0 :l),m++){
+						list.set(l, toReverseList.get(m));
 					}
-					basePointIndex=0;
+					i=0; // reset
 				}
 			}
 		}
+
 	}
-	
+
 	@Override
 	public String toString(){
 		return "2-opt Method";
